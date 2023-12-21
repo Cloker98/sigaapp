00146b31d53ec2c5300a1e65c4be75809b7fb5f2
Elizeu Grosskopf Schlottfeldt Neto <42324527+Cloker98@users.noreply.github.com>
2023-12-21 09:53:34 -0300

    changing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
-import { IonicModule } from '@ionic/angular';
-import { IonicStorageModule } from '@ionic/storage-angular';
+// app.module.ts
+import { Component, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { FormsModule } from '@angular/forms';
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  // component logic
+}
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    IonicModule.forRoot(),
-    IonicStorageModule.forRoot(),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production
-    })
+  declarations: [
+    // remove 'AppComponent' from declarations
   ],
-  declarations: [AppComponent],
-  providers: [InAppBrowser],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [],
 })
 export class AppModule {}
